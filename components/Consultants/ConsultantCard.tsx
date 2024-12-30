7454ff4934cf7e8fcbb8080018986fd55e262174
Kourosh <kouroshsharifineyestani@gmail.com>
2024-12-30 16:08:45 +0330

    add advisor/consultant single page

---
+++ components/Consultants/ConsultantCard.tsx
@@ -0,0 +1,118 @@
+import { fakeVilla, fakeConsultant } from "@/utils/faker";
+import _STRINGS from "@/utils/LocalStrings";
+import React from "react";
+// import AddCardPricePart from "./AddCardPricePart";
+import Link from "next/link";
+import Image from "next/image";
+
+
+
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const ConsultantCard = () => {
+    const data = fakeConsultant;
+    return (
+        <div className="w-full shadow-card rounded-2xl p-4 gap-0 md:gap-2">
+            <Link href={`/consultants/${data?.id}`}>
+                <div className="w-full flex items-center justify-between gap-6">
+                    <div className="w-2/6">
+                        <div className="flex flex-col gap-2">
+                            <div className="relative rounded-full bg-gray-300 overflow-hidden">
+                                {data?.avatar ? (
+                                    <Image
+                                        alt={"#"}
+                                        src={data?.avatar}
+                                        width={300}
+                                        height={300}
+                                        className="w-full h-full aspect-square object-cover object-top"
+                                    />
+                                ) : (
+                                    <div className="absolute inset-0">
+                                        place holder image
+                                    </div>
+                                )}
+                            </div>
+                            {/* CODE  - LIKES */}
+                            <div className="flex items-center justify-center gap-4">
+                                <div className="bg-primary-700 rounded-md text-xs  px-2 py-1 text-white flex items-center justify-center">
+                                    کد {data.code}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <aside className="w-full">
+                        <div className="flex flex-col gap-4">
+                            {/* TITLE */}
+                            <div className="flex items-center gap-2">
+                                <p className="text-xl font-semibold">
+                                    {data.name}
+                                </p>
+                            </div>
+                            {/* CIRCULAR BARS */}
+                            <div className="flex items-center gap-4">
+                                <div className="flex flex-col sm:flex-row items-center gap-2 relative text-[#3886E5]">
+                                    <span className="text-nowrap font-semibold">
+                                        رضایت کاربران
+                                    </span>
+                                    <div className="w-12 relative">
+                                        <CircularProgressbar
+                                            value={data?.users_satisfaction}
+                                            text={data?.users_satisfaction.toString()}
+                                            styles={buildStyles({
+                                                textColor: "#3886E5",
+                                                pathColor: "#3886E5",
+                                                textSize: "2.4rem",
+                                            })}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="flex flex-col sm:flex-row items-center gap-2 relative text-[#34C759]">
+                                    <span className="text-nowrap font-semibold">
+                                        رضایت کاربران
+                                    </span>
+                                    <div className="w-12 relative">
+                                        <CircularProgressbar
+                                            value={data?.owners_satisfaction}
+                                            text={data?.owners_satisfaction.toString()}
+                                            styles={buildStyles({
+                                                textColor: "#34C759",
+                                                pathColor: "#34C759",
+                                                textSize: "2.4rem",
+                                            })}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            {/* LOCATION */}
+                            <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+                                <div className="flex items-center">
+                                    <span>حوزه فعالیت:</span>
+                                    <ul className="flex flex-wrap list-none">
+                                        {data?.locations.map((item, index) => (
+                                            <li className="" key={index}>
+                                                {`${item} ${
+                                                    index <=
+                                                    data.locations.length - 2
+                                                        ? " و "
+                                                        : ""
+                                                } `}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+
+                                <div>
+                                    <span>سابقه: </span>
+                                    <span>6 ماه</span>
+                                </div>
+                            </div>
+                        </div>
+                    </aside>
+                </div>
+            </Link>
+        </div>
+    );
+};
+
+export default ConsultantCard;
