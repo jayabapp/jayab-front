578e58940992b8e4baa05ec7ed339239449dad0a
MohammadrasiM <benkenobl@yahoo.com>
2025-02-15 12:46:44 +0330

    fix: update onBackgroundMessage to use await for proper async handling

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -21,7 +21,7 @@ firebase.initializeApp({
 // messages.
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function (payload) {
+await messaging.onBackgroundMessage(function (payload) {
   console.log("Received background message ", payload);
   // Customize notification here
   const notificationTitle = payload.notification.title;
