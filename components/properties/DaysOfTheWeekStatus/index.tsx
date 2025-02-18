7ac885da2a32ca9aa6e9de418c775b930c74c493
MohammadrasiM <benkenobl@yahoo.com>
2025-02-18 13:37:32 +0330

    fix: comment out unused paragraph element in DaysOfTheWeekStatus for cleaner code

--- components/properties/DaysOfTheWeekStatus/index.tsx
+++ components/properties/DaysOfTheWeekStatus/index.tsx
@@ -21,12 +21,12 @@ const DaysOfTheWeekStatus = ({ isCard, data, week }: { week: any[]; isCard?: boo
           key={`days${e?.id}${e?.title}`}
           className=" "
         >
-          <p
+          {/* <p
             style={{ textAlign: "center" }}
             className={`text-xxs   !shrink-0  ${isCard ? "" : "md:text-xs"}   text-gray-400 `}
           >
             {e?.title}
-          </p>
+          </p> */}
 
           <div
             className={`  ${
