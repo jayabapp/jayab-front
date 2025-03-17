4b4ee80cf4fb907704ba7ac226e7d2d7ba840675
MohammadrasiM <benkenobl@yahoo.com>
2025-03-17 18:09:42 +0330

    feat: revalidator updated

--- helpers/MinMaxRandom.ts
+++ helpers/MinMaxRandom.ts
@@ -1,6 +1,6 @@
 const MinMaxRandom = () => {
-  const max = 60;
-  const min = max - 20;
+  const max = 300;
+  const min = max - 60;
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
