13de0e32513ff9b51a61d6ebef0ba02b6a7581e4
MohammadrasiM <benkenobl@yahoo.com>
2025-04-21 10:23:52 +0330

    fix: update z-index in FixedBottomContainer for improved layering

--- components/shared/FixedBottomContainer/index.tsx
+++ components/shared/FixedBottomContainer/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const FixedBottomContainer = ({ children }: { children: any }) => {
   return (
     <div
-      className={`z-1  flex   max-w-[800px]  pb-8  pt-3 md:py-3   justify-between  md:rounded-md  left-0  right-0     mx-auto   shadow-card transition-all duration-1000	ease-in-out  items-center fixed bottom-0 w-full   bg-white    dark:border-t `}
+      className={`z-[11]  flex   max-w-[800px]  pb-8  pt-3 md:py-3   justify-between  md:rounded-md  left-0  right-0     mx-auto   shadow-card transition-all duration-1000	ease-in-out  items-center fixed bottom-0 w-full   bg-white    dark:border-t `}
     >
       {children}
     </div>
