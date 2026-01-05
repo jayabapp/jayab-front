ae8f6219f93d3b81a183abae112a254bfc793a3c
mohammad rasi <benkenobl@yahoo.com>
2026-01-05 17:03:29 +0330

    fix: google tag script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,15 @@ const RootLayout = async ({
         />
         <script
           dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${appSetting?.googleTagManagerId?.toString()}');`,
+          }}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
             __html: `!function (t, e, n) {
 t.yektanetAnalyticsObject = n, t[n] = t[n] || function () {
 t[n].q.push(arguments)
