ad9b8e028424be3e23a81d03c6571f4e397395e1
mohammad rasi <benkenobl@yahoo.com>
2026-06-07 10:24:02 +0330

    feat: added initial og tags to metaHeaderHelper

--- helpers/MetaHeaderHelper.ts
+++ helpers/MetaHeaderHelper.ts
@@ -1,13 +1,26 @@
+import { NEW_IMAGE_URL } from "@/utils/urls";
 import { headers } from "next/headers";
-import React from "react";
+
 const MetaHeaderHelper = async (data: any) => {
   const requestHeaders = await headers();
   const xCanonical = await requestHeaders?.get("x-canonical");
+
+  const title = data?.seo?.metaTitle || data?.title;
+  const description = data?.seo?.metaDescription || data?.full_text || data?.title;
+  const canonicalUrl = data?.seo?.canonicalURL || xCanonical;
+  const ogImage = NEW_IMAGE_URL(data?.feature_image) || "";
+
   return {
-    title: data?.seo?.metaTitle || data?.title,
-    description: data?.seo?.metaDescription || data?.full_text || data?.title,
+    title,
+    description,
     alternates: {
-      canonical: data?.seo?.canonicalURL || xCanonical,
+      canonical: canonicalUrl,
+    },
+    openGraph: {
+      title: title,
+      description: description,
+      url: canonicalUrl,
+      image: ogImage || undefined,
     },
   };
 };
