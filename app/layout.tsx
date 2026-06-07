e125fac0b75f555276d6faf28d3332449107a356
mohammad rasi <benkenobl@yahoo.com>
2026-06-07 14:01:54 +0330

    fix: layout og:title removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,7 +79,6 @@ s.src = "https://cdn.yektanet.com/rg_woebegone/scripts_v3/Sfsc56h6/rg.complete.j
           }}
         />
         <meta name="keywords" content="جایاب" />
-        <meta property="og:title" content="جایاب" />
         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
